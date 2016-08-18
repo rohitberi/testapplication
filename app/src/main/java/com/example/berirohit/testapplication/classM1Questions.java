81f04587c71dce5b8b1ac12ea337e9931dd1a3b4
Rohit Beri <rohitberi@gmail.com>
2016-08-18 09:16:29 -0400

    added a few new activities (menu and m1Questions)

--- app/src/main/java/com/example/berirohit/testapplication/classM1Questions.java
+++ app/src/main/java/com/example/berirohit/testapplication/classM1Questions.java
@@ -11,33 +11,126 @@ public class classM1Questions {
     private final String LOGTYPE = "INFORMATION";
     private static int iMaxQuestions = 5;
 
-    // these variables relate to the question that is in memory / being displayed on the UI
+    private String sQuestion = "";
     private String sAnswer = "";
+    private String sAnswer1 = "";
+    private String sAnswer2 = "";
+    private String sAnswer3 = "";
+    private String sAnswer4 = "";
+
+    // these variables relate to the question that is in memory / being displayed on the UI
     private int iQuestion = 0;
 
     protected void classM1Questions(){
         iCounter = 0;
     }
 
-    void nextQuestion(){
-        Log.i(LOGTYPE, "function: nextQuestion: " + String.valueOf(iCounter));
+    void firstQuestion(){
 
-        if(iCounter < iMaxQuestions){
-            readQuestion(iCounter);
-            iCounter ++;
-        }
-        Log.i(LOGTYPE, "function: nextQuestion: " + String.valueOf(iCounter));
+        iCounter = 1;
+        Log.i(LOGTYPE, "function: previousQuestion: " + String.valueOf(iCounter));
+        readQuestion(iCounter);
     }
 
-    void previousQuestion(){
+    Boolean nextQuestion(){
+        iCounter ++;    // counter starts at 0; before reading next question we move the counter up by 1
+        Log.i(LOGTYPE, "function: nextQuestion: " + String.valueOf(iCounter));
+
+        if(iCounter <= iMaxQuestions){
+            readQuestion(iCounter);
+            return true;
+        }else{
+            iCounter --;  // reset back to last question counter
+            Log.i(LOGTYPE, "function: nextQuestion: End of Questions reached");
+            return false;
+        }
+    }
+
+    Boolean previousQuestion(){
 
         iCounter --;
         Log.i(LOGTYPE, "function: previousQuestion: " + String.valueOf(iCounter));
+        if (iCounter > 0){
+            readQuestion(iCounter);
+            return true;
+        }else{
+            iCounter ++;  // reset back to last question counter
+            Log.i(LOGTYPE, "function: previousQuestion: End of Questions reached");
+            return false;
+        }
     }
 
     void readQuestion(int iCount){
         // this function will read the contents of a question into memory
+        Log.i(LOGTYPE, "function: readQuestion: " + String.valueOf(iCounter));
+        switch (iCount){
+            case 1 :
+                sQuestion =  "This is Question 1"; //R.array.m1Q1[0];
+                sAnswer = "A";
+                sAnswer1 = "Answer 1 for Question 1";
+                sAnswer2 = "Answer 2 for Question 1";
+                sAnswer3 = "Answer 3 for Question 1";
+                sAnswer4 = "Answer 4 for Question 1";
+            break;
+
+            case 2:
+                sQuestion =  "This is Question 2"; //R.array.m1Q1[0];
+                sAnswer = "A";
+                sAnswer1 = "Answer 1 for Question 2";
+                sAnswer2 = "Answer 2 for Question 2";
+                sAnswer3 = "Answer 3 for Question 2";
+                sAnswer4 = "Answer 4 for Question 2";
+                break;
+
+            case 3:
+                sQuestion =  "This is Question 3"; //R.array.m1Q1[0];
+                sAnswer = "A";
+                sAnswer1 = "Answer 1 for Question 3";
+                sAnswer2 = "Answer 2 for Question 3";
+                sAnswer3 = "Answer 3 for Question 3";
+                sAnswer4 = "Answer 4 for Question 3";
+                break;
+
+            case 4:
+                sQuestion =  "This is Question 4"; //R.array.m1Q1[0];
+                sAnswer = "A";
+                sAnswer1 = "Answer 1 for Question 4";
+                sAnswer2 = "Answer 2 for Question 4";
+                sAnswer3 = "Answer 3 for Question 4";
+                sAnswer4 = "Answer 4 for Question 4";
+                break;
+
+            case 5:
+                sQuestion =  "This is Question 5"; //R.array.m1Q1[0];
+                sAnswer = "A";
+                sAnswer1 = "Answer 1 for Question 5";
+                sAnswer2 = "Answer 2 for Question 5";
+                sAnswer3 = "Answer 3 for Question 5";
+                sAnswer4 = "Answer 4 for Question 5";
+                break;
+        }
     }
+
+    public String get_sQuestion(){
+        return sQuestion;
+    }
+
+    public String get_sAnswer(){
+        return sAnswer;
+    }
+    public String get_sAnswer1(){
+        return sAnswer1;
+    }
+    public String get_sAnswer2(){
+        return sAnswer2;
+    }
+    public String get_sAnswer3(){
+        return sAnswer3;
+    }
+    public String get_sAnswer4(){
+        return sAnswer4;
+    }
+
 
 }
 
--- app/src/main/java/com/example/berirohit/testapplication/m1questions.java
+++ app/src/main/java/com/example/berirohit/testapplication/m1questions.java
@@ -2,11 +2,19 @@ package com.example.berirohit.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class m1questions extends AppCompatActivity {
 
     classM1Questions oQuestions;
+    ImageView imgPrevQuestion;
+    ImageView imgNextQuestion;
+    TextView txtbottom;
+    private final String LOGTYPE = "INFORMATION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,39 @@ public class m1questions extends AppCompatActivity {
         setContentView(R.layout.activity_m1questions);
 
         oQuestions = new classM1Questions();
-        oQuestions.NextQuestion();
+        oQuestions.firstQuestion();
+
+        imgPrevQuestion = (ImageView) findViewById(R.id.imgPrevQuestion);
+        imgNextQuestion = (ImageView) findViewById(R.id.imgNextQuestion);
+        txtbottom = (TextView) findViewById(R.id.txtbottom);
+
+        imgPrevQuestion.setOnClickListener(new OnImageButtonClick());
+        imgNextQuestion.setOnClickListener(new OnImageButtonClick());
+    }
+
+    public class OnImageButtonClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            Boolean bContinue = false;
+            switch (view.getId()) {
+                case R.id.imgPrevQuestion:
+                    Log.i(LOGTYPE, "case: imgPrevQuestion");
+                    bContinue = oQuestions.previousQuestion();
+                    if (bContinue == true){
+                        // set all question text and answers
+                    }
+                    break;
+
+                case R.id.imgNextQuestion:
+                    Log.i(LOGTYPE, "case: imgNextQuestion");
+                    bContinue = oQuestions.nextQuestion();
+                    if (bContinue == true){
+                        // set all question text and answers
+                    }
+                    break;
+
+            }
+        }
     }
 }
